18c9ac269511b8def20e15d3840b49469e2ae646
locdh <dghuuloc@gmail.com>
2025-12-18 17:50:47 +0700

    edit colorscheme

--- init.lua
+++ init.lua
@@ -3,12 +3,8 @@
 -- author: dghuuloc
 -- ================================================================================================-------------------------------------------------------------------------------------------------
 
--------------------------------------------------------------------------------------------------
--- THEME
--------------------------------------------------------------------------------------------------
--- Change default colorscheme to 'vim'
--- vim.cmd.colorscheme('habamax')
-pcall(vim.cmd.colorscheme, 'unokai')
+-- theme
+vim.cmd.colorscheme('unokai')
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
