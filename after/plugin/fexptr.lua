5130272e41c838289e07dad17ba9e07001998d88
Duong Huu Loc <87122072+dghuuloc@users.noreply.github.com>
2026-04-03 20:59:15 +0700

    Update fexptr.lua

--- after/plugin/fexptr.lua
+++ after/plugin/fexptr.lua
@@ -1,6 +1,17 @@
 require("fexptr").setup({
     width = 35,
     show_hidden = false,
+
+    folder_indicators = {
+        open = "▾",
+        closed = "▸",
+    },
+
+    icons = {
+        folder_open = "",
+        folder_closed = "",
+        file = "󰈙",
+    },
     -- icons = {
     --     folder_closed = "",
     --     folder_open   = "",
