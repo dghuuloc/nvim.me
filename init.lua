43305d974b09e1c20d3e12953b41a3376de05dbd
Duong Huu Loc <87122072+dghuuloc@users.noreply.github.com>
2026-04-13 20:28:24 +0700

    comment nvim-lspconfig

--- init.lua
+++ init.lua
@@ -140,7 +140,7 @@ vim.pack.add({
     { src = "https://github.com/tpope/vim-fugitive" },
 
     -- #LSP + Completion
-    { src = "https://github.com/neovim/nvim-lspconfig" },
+    -- { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/mason-org/mason.nvim" },
     {
       src     = "https://github.com/saghen/blink.cmp",
